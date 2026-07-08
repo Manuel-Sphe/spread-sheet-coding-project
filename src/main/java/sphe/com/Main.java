a43d2ae41a53ba232068c1c6ae3c94eae71a475a
Manuel-Sphe <sphesihlemanuel99@gmail.com>
2026-07-08 11:36:37 +0200

    Reading input file

--- src/main/java/sphe/com/Main.java
+++ src/main/java/sphe/com/Main.java
@@ -1,7 +1,29 @@
 package sphe.com;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+        // file input reading
+        try (InputStream in = Main.class.getClassLoader()
+                .getResourceAsStream("input.csv")) {
+            if (in == null) {
+                throw new IllegalArgumentException("file not found");
+            }
+            try (BufferedReader br = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8))) {
+
+                String line;
+                while ((line = br.readLine()) != null) {
+                    String[] split = line.split(",");
+                    System.out.println(split[0] + " " + split[1]);
+                }
+            }
+        }
 
     }
 }
